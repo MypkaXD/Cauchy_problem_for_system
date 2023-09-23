58c8dc1c3bb1d2616fc9b493c43f22149c088e20
MypkaXD <nicklayschannel@yandex.ru>
2023-09-23 20:45:42 +0300

    added rk4 ver 0.1

--- source/Cauchy_problem_for_system.cpp
+++ source/Cauchy_problem_for_system.cpp
@@ -8,38 +8,52 @@
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(640,480, 32), "ImGui + SFML = <3");
-    window.setFramerateLimit(60);
-    ImGui::SFML::Init(window);
+#include <vector>
+#include <iostream>
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+double getNewX(double x, double h,double a, int i) {
 
-    sf::Clock deltaClock;
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            ImGui::SFML::ProcessEvent(window, event);
+    x = a + h * i;
 
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
+    return x;
+}
 
-        ImGui::SFML::Update(window, deltaClock.restart());
+double func(double x, double y) {
+    return (2 * x - y + x * x);
+}
 
-        ImGui::ShowDemoWindow();
-
-        ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
-        ImGui::End();
-
-        window.clear();
-        window.draw(shape);
-        ImGui::SFML::Render(window);
-        window.display();
+void run(std::vector<std::pair<double, double>>& Table, double a, double b, double h) {
+    int count = 0;
+    Table.push_back({ getNewX(0, h, a, count), 0 });
+    while (Table[count].first <= b) {
+        std::cout << Table[count].first << "||||" << Table[count].second << std::endl;
+        Table[count+1].first = getNewX(Table[count].first, h, a, count);
+        double c0 = func(Table[count].first, Table[count].second);
+        double c1 = func(Table[count].first + h / 2, Table[count].second + h * c0 / 2);
+        double c2 = func(Table[count].first + h / 2, Table[count].second + h * c1 / 2);
+        double c3 = func(Table[count].first + h / 2, Table[count].second + h * c2);
+        Table[count+1].second = (Table[count].second + h / 6 * (c0 + 2 * c1 + 2 * c2 + c3));
+        ++count;
     }
+}
 
-    ImGui::SFML::Shutdown();
+
+int main() {
+    double a, b;
+
+
+
+    std::vector<std::pair<double,double>> Table(100);
+    double t;
+    double h;
+    int n;
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> n;
+    h = (a + b) / n;
+
+    run(Table, a, b, h);
+
+
+    return 0;
 }
