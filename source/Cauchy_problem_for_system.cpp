e6129dbcb1521d862f7573eadbc123091955ef12
MypkaXD <nicklayschannel@yandex.ru>
2023-09-23 16:42:12 +0300

    Added files and struct

---
+++ source/Cauchy_problem_for_system.cpp
@@ -0,0 +1,45 @@
+﻿#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include "imconfig.h"
+#include "imconfig-SFML.h"
+
+#include "SFML/Graphics.hpp"
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640,480, 32), "ImGui + SFML = <3");
+    window.setFramerateLimit(60);
+    ImGui::SFML::Init(window);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Clock deltaClock;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(window, event);
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Hello, world!");
+        ImGui::Button("Look at this pretty button");
+        ImGui::End();
+
+        window.clear();
+        window.draw(shape);
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+}
