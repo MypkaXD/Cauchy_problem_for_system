940f08bef1c90c76a39203763644efbcc901b1f3
MypkaXD <nicklayschannel@yandex.ru>
2023-09-24 11:16:00 +0300

    Added class RKIV

--- source/Cauchy_problem_for_system.cpp
+++ source/Cauchy_problem_for_system.cpp
@@ -11,61 +11,58 @@
 #include <vector>
 #include <iostream>
 
-double getNewX(double x, double h,double a, int i) {
 
-    x = a + h * i;
+class RK_IV {
+private:
+    std::vector<std::pair<double, double>> m_data; // хранит точки (x,y)
+    double m_h = 10e-5; // численный шаг интегрирования
+    int m_N_max = 10000; // 
+    double m_E_check_right = 0.5*10e-6;
+    double m_E_check_up = 0.5 * 10e-4;
+    double m_E_check_down = m_E_check_up / (2 >> 4);
 
-    return x;
-}
+    double a = 0;
+    double b = 0.2;
 
-double func(double x, double y) {
-    return (2 * x - y + x * x);
-}
+public:
 
-void run(std::vector<std::pair<double, double>>& Table, double a, double b, double h) {
-    int count = 0;
-    Table.push_back({ getNewX(0, h, a, count), 0 });
-    while (Table[count].first <= b) {
-        std::cout << Table[count].first << "||||" << Table[count].second << std::endl;
-        Table[count+1].first = getNewX(Table[count].first, h, a, count);
-        double c0 = func(Table[count].first, Table[count].second);
-        double c1 = func(Table[count].first + h / 2, Table[count].second + h * c0 / 2);
-        double c2 = func(Table[count].first + h / 2, Table[count].second + h * c1 / 2);
-        double c3 = func(Table[count].first + h / 2, Table[count].second + h * c2);
-        Table[count+1].second = (Table[count].second + h / 6 * (c0 + 2 * c1 + 2 * c2 + c3));
-        ++count;
+    RK_IV() {
+        m_data.push_back({ 0,0 });
     }
-}
 
-bool checkUp(double y) {
+    double getNewX(int count) {
+        return ( a + m_h * count);
+    }
 
-}
+    double func(double x, double y) {
+        return (2 * x - y + x * x);
+    }
 
+    void run() {
+        int count = 0;
+        while (m_data[count].first <= b) {
+            double c0 = func(m_data[count].first, m_data[count].second);
+            double c1 = func(m_data[count].first + m_h / 2, m_data[count].second + m_h * c0 / 2);
+            double c2 = func(m_data[count].first + m_h / 2, m_data[count].second + m_h * c1 / 2);
+            double c3 = func(m_data[count].first + m_h / 2, m_data[count].second + m_h * c2);
+            m_data.push_back({ getNewX(count), m_data[count].second + m_h / 6 * (c0 + 2 * c1 + 2 * c2 + c3) });
+            ++count;
+        }
+    }
+
+    void print() {
+        int count = 0;
+        while (!m_data.empty()) {
+            std::cout << "x" << count << " = " << m_data[count].first << "\t|\ty" << count << " = " << m_data[count].second << std::endl;
+            ++count; 
+        }
+    }
+};
 
 int main() {
-    double a, b;
-
-
-
-    std::vector<std::pair<double,double>> Table(100);
-    double t;
-    double h = 10e-5;
-    int N = 10e3;
-    double e_right = 0.5 * 10e-6;
-    double e_up = 0.5*10e-4;
-    double e_min = e_up / (2 << 4);
-    std::cout << (2 << 4) << std::endl;
-    std::cout << h << std::endl;
-    std::cout << e_min << std::endl;
-
-    int n;
-    std::cin >> a;
-    std::cin >> b;
-    std::cin >> n;
-    h = (a + b) / n;
-
-    run(Table, a, b, h);
-
+    RK_IV rkiv;
+    rkiv.run();
+    rkiv.print();
 
     return 0;
 }
