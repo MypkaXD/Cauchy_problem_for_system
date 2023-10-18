a4dbba593427852becc2b6411e76ff4dbf9da646
MypkaXD <nicklayschannel@yandex.ru>
2023-10-18 11:21:55 +0300

    added comments

--- include/RKIV.h
+++ include/RKIV.h
@@ -27,7 +27,7 @@ private:
     int m_N_max = 100; // максимальное число шагов
     int p = 4; // порядок метода
 
-    std::vector<double> vector_of_h;
+    std::vector<double> vector_of_h; // веткор для хранения шагов
 
 public:
 
@@ -39,7 +39,7 @@ public:
     double test_func(double x, double u) { // реализация функции du/dx для тестовой задачи
         // У нас 5-ый №команды => du/dx = (-1)^5*5/2*u => du/dx = -5/2*u
 
-        return (log(x+1)/(x*x+1));
+        return (-5/2*u);
     }
 
     double getNewX(double x, double h) { // получаем новый x
@@ -75,101 +75,72 @@ public:
         
         double S = abs(getS(u_with_h, u_with_twice_half_h)); // получаем S и сразу берем от неё модуль
 
-        std::cout << m_E_check_down << std::endl;
-        std::cout << S << std::endl;
+        //std::cout << m_E_check_down << std::endl;
+        //std::cout << S << std::endl;
 
-        if (S >= m_E_check_down && S <= m_E_check_up) {
-            std::cout << "ALL GOOD" << std::endl;
+        if (S >= m_E_check_down && S <= m_E_check_up) { // если S находится в границе "локальной погрешности" то возвращаем статус NOTHING
             return Actions_with_H::NOTHING;
         }
-        else if (S < m_E_check_down) {
-            std::cout << "H * 2" << std::endl;
+        else if (S < m_E_check_down) { // если S находится ниже нижней границы, то возвращаем статус MULTIPLY_BY_2
             return Actions_with_H::MULTIPLY_BY_2;
         }
-        else if (S > m_E_check_up) {
-            std::cout << "H / 2" << std::endl;
+        else if (S > m_E_check_up) { // если S находится выше верхней границы, то возвращаем статус DIVIDE_BY_2_AND_RECALCULATE
             return Actions_with_H::DIVIDE_BY_2_AND_RECALCULATE;
         }
     }
 
     Actions_with_H checkRight(double x) {
         if (x > m_b)
-            return Actions_with_H::GET_LAST;
+            return Actions_with_H::GET_LAST; // если мы попали за правую границу, то нам необходимо вернуться и получить последний элемент
         else if (x == m_b)
-            return Actions_with_H::STOP;
+            return Actions_with_H::STOP; // если мы попали ровно на правую границу, то мы получаем последний элемент и заканчиваем
         else if (x <= m_b - m_E_check_right)
-            return Actions_with_H::NOTHING;
+            return Actions_with_H::NOTHING; // если мы попали левее эпсилон-правой-границы, то ничего не делаем и продолжаем работу
     }
 
     std::vector<std::pair<double, double>> run(double x0, double u0) {
-        std::vector<std::pair<double, double>> data;
+        std::vector<std::pair<double, double>> data; // создаем вектор data для хранения точек x и u
         
-        data.push_back({ x0,u0 });
+        data.push_back({ x0,u0 }); // пушим в вектор начальные условия
 
-        Actions_with_H act = Actions_with_H::NOTHING;
-        std::pair<double, double> coords_with_h;
-
-        int k = 0;
+        std::pair<double, double> coords_with_h; // создаем вектор coords_with_h для хранения точек x и u с шагом h
 
         while (true) {
-            std::pair<double, double> current_coord = data.back();
+            std::pair<double, double> current_coord = data.back(); // получаем текущие координаты (x_n, u_n)
             
-            coords_with_h = RKIV(current_coord.first, current_coord.second, m_h);
-            std::pair<double, double> coords_with_half_h = RKIV(current_coord.first, current_coord.second, m_h / 2);
-            std::pair<double, double> coords_with_twice_half_h = RKIV(coords_with_half_h.first, coords_with_half_h.second, m_h / 2);
+            coords_with_h = RKIV(current_coord.first, current_coord.second, m_h); // получаем точку (x_n+1, u_n+1) с шагом h из точки (x_n, u_n)
+            std::pair<double, double> coords_with_half_h = RKIV(current_coord.first, current_coord.second, m_h / 2); //  получаем точку (x_n+1/2, u_n+1/2) из точки (x_n, u_n) с шагом h/2
+            std::pair<double, double> coords_with_twice_half_h = RKIV(coords_with_half_h.first, coords_with_half_h.second, m_h / 2); // получаем точку (x_n+1, u_n+1) из точки (x_n+1/2, u_n+1/2) с шагом h / 2
 
 
-            Actions_with_H act1 = checkUpDown(coords_with_h.second, coords_with_twice_half_h.second);
-            Actions_with_H act2 = checkRight(coords_with_h.first);
+            Actions_with_H act1 = checkUpDown(coords_with_h.second, coords_with_twice_half_h.second); // проверка на выход за локальную погрешность
+            Actions_with_H act2 = checkRight(coords_with_h.first); // проверка за выход за правую границу
 
             if (act2 == Actions_with_H::GET_LAST) {
-                m_h = m_b - data.back().first;
+                m_h = m_b - data.back().first; // устанавливаем шаг, который привидет нас точно в правую грницу m_b
             }
             else if (act2 == Actions_with_H::STOP) {
-                data.push_back(coords_with_h);
-                vector_of_h.push_back(m_h);
-                std::cout << "STOP" << std::endl;
+                data.push_back(coords_with_h); // сохраняем точку
+                vector_of_h.push_back(m_h); // сохраняем шаг
                 break;
             }
-            else if (act2 == Actions_with_H::NOTHING) {
-                if (act1 == Actions_with_H::MULTIPLY_BY_2) {
-                    data.push_back(coords_with_h);
-                    vector_of_h.push_back(m_h);
-                    m_h *= 2;
-                    std::cout << "MULTIPLY_BY_2" << std::endl;
-                    --m_N_max;
+            else if (act2 == Actions_with_H::NOTHING) { // если мы не вышли за правую границу - вернулся статус NOTHING
+                if (act1 == Actions_with_H::MULTIPLY_BY_2) { // если небходимо шаг умножить на два после проверки на лок. погрешность
+                    data.push_back(coords_with_h); // сохраняем точку
+                    vector_of_h.push_back(m_h); // сохраняем шаг
+                    m_h *= 2; // умножаем шаг на 2
                 }
-                else if (act1 == Actions_with_H::NOTHING)
+                else if (act1 == Actions_with_H::NOTHING) // если нам ничего не надо делать с шагом, находимся ровно в границе лок. погрешности
                 {
-                    data.push_back(coords_with_h);
-                    vector_of_h.push_back(m_h);
-                    std::cout << "NOTHING" << std::endl;
-                    --m_N_max;
+                    data.push_back(coords_with_h); // сохраняем точку
+                    vector_of_h.push_back(m_h); // сохраняем шаг
                 }
-                else if (act1 == Actions_with_H::DIVIDE_BY_2_AND_RECALCULATE) {
-                    m_h /= 2;
-                    std::cout << "DIVIDE_BY_2_AND_RECALCULATE" << std::endl;
-                }
+                else if (act1 == Actions_with_H::DIVIDE_BY_2_AND_RECALCULATE) // если нам надо поделить шаг на два и пересчитать точку
+                    m_h /= 2; // делим шаг на два
             }
 
-            std::cout << "+++++++++++++++++++" << std::endl;
-
         }
-        
-        for (int count = 0; count < data.size(); ++count)
-            std::cout << data[count].first << "\t " << data[count].second << std::endl;
 
-        std::cout << "+++++++++++++++++++" << std::endl;
-
-        for (int count = 0; count < vector_of_h.size(); ++count)
-            std::cout << vector_of_h[count] << std::endl;
-
-        return data;
+        return data; // возвращаем вектор точек
     }
 };
-
-/*
-
-Не забудь починить хрень с выходом за правую границу, чтобы мы не ошивались у m_b пока N > 0
-
-*/
--- source/Cauchy_problem_for_system.cpp
+++ source/Cauchy_problem_for_system.cpp
@@ -16,13 +16,11 @@
 
 int main() {
     
+    system("chcp 1251");
+
     App app;
     app.init();
     app.run();
-    //
-    //double m_E_check_up = 0.5 * 10e-4; // проверка для "вверха"
-    //double m_E_check_down = m_E_check_up / 15; // проверка для "низа"
-    //std::cout << m_E_check_up << std::endl;
-    //std::cout << m_E_check_down << std::endl;
+    
     return 0;
 }
